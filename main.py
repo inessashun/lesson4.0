288c1e24a47a2331cc6c0d7271de0419fe83b667
inessashun <inshun2018@gmail.com>
2024-05-08 18:36:26 +0300

    add conflict codes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
-print("conflict")
-
+print("conflict1")
 
 # a = 7   #create variable
 # b = 9
@@ -8,12 +7,12 @@ print("conflict")
 for i in range(8):
     print("word")
 a, b = 9, 8
-
+print("conflict2")
 if a > b:
     print("a bigger than b")
 elif b > a:
     print("b bigger than a")
 else:
     print("a equal to b")
-
+print("conflict3")
     
